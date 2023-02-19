83a04fa91cad14d60e2d7be7c893b2b990f2e595
Varsha <varsharimjhim51@gmail.com>
2023-02-19 14:11:32 +0530

    sc

---
+++ RockPaperScissors.py
@@ -0,0 +1,21 @@
+import random
+
+x = 'y'
+while  x.lower() == 'y':
+    user = input("Your Turn : Enter your Choice (rock, paper, scissor):")
+    moves = ["rock","paper","scissor"]
+    computer = random.choices(moves)
+    print( "Computer turn: ", *computer)
+    if user == computer :
+        print("Its a Tie")
+
+    elif user == "rock" and computer == "scissor" or user == "rock" and computer == "paper" or user == "paper" and computer == "rock" or user == "scissor" and computer == "paper":
+        print("Hurray :) ----->> VICTORY ")
+
+    else:
+        print("Better Luck Next Time :(")
+
+    x = input("Do you want to continue (y/n)")
+
+
+
